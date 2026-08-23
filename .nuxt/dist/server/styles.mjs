b313aa284e22c100605404830f18867e46b51d31
yasirmaulana <yasir.maulana@gmail.com>
2026-08-23 19:30:30 +0700

    fix: prisma ke dependencies, tambah prisma generate saat build, preset vercel

--- .nuxt/dist/server/styles.mjs
+++
@@ -1,6 +0,0 @@
-const interopDefault = r => r.default || r || []
-export default {
-  "node_modules/nuxt/dist/app/entry.js": () => import('./_nuxt/entry-styles.GMpV_7pF.mjs').then(interopDefault),
-  "node_modules/nuxt/dist/app/components/error-404.vue": () => import('./_nuxt/error-404-styles.Pd4Strki.mjs').then(interopDefault),
-  "node_modules/nuxt/dist/app/components/error-500.vue": () => import('./_nuxt/error-500-styles.BMmh3bAG.mjs').then(interopDefault)
-}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,6 +30,7 @@ export default defineNuxtConfig({
   },
 
   nitro: {
+    preset: 'vercel',
     experimental: {
       wasm: false
     }
